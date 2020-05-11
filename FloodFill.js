eb8717a45670b98414789ae3083422839c697981
arunkumars0807 <62843045+arunkumars0807@users.noreply.github.com>
2020-05-11 22:10:25 +0530

    Create FloodFill.js

---
+++ FloodFill.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor) {
+    var result = dfs(image, sr, sc, image[sr][sc], newColor);
+    if (result === null) {
+        return image;
+    }
+    return result;
+};
+
+var dfs = function(arr, i, j, src, tgt) {
+    if (i < 0 || j < 0 || i >= arr.length || j >= arr[i].length || arr[i][j] === tgt || arr[i][j] !== src) {
+        return null;
+    }
+    
+    arr[i][j] = tgt;
+    
+    if (i < arr.length - 1 && arr[i + 1][j] === src) {
+        dfs(arr, i + 1, j, src, tgt);
+    }
+    
+    if (i > 0 && arr[i - 1][j] === src) {
+        dfs(arr, i - 1, j, src, tgt);
+    }
+        
+    if (j < arr[i].length - 1 && arr[i][j + 1] === src) {
+        dfs(arr, i, j + 1, src, tgt);
+    }
+        
+    if (j > 0 && arr[i][j - 1] === src) {
+        dfs(arr, i, j - 1, src, tgt);
+    }
+    
+    return arr;
+}
