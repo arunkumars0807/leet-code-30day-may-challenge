8e591b6a09c6770ed12550d265b4d79a4be5684c
arunkumars0807 <62843045+arunkumars0807@users.noreply.github.com>
2020-05-17 10:12:12 +0530

    Create OddEvenLinkedList.js

---
+++ OddEvenLinkedList.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    
+    var odd = head;
+    var even = head.next;
+    var tempEven = even;
+
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+
+    odd.next = tempEven;
+    return head;
+};
